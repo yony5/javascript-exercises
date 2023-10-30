1e622b2d802c6be22f91bb2e264508c4eff4ce53
yony fernandez  (personal) <yonyfernandezjr@gmail.com>
2023-10-30 00:38:55 -0400

    Completed Unit Test 1 & 2
    testing and learning javascript unit testing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = (word, number) => {
+    let i =0;
+    let result="";
 
-};
+    if(number < 0){
+        return "ERROR";
+    }
+    while(i < number){
+          result += word;
+          console.log(word);
+          i++
+      }
+        return result;
+    };
 
 // Do not edit below this line
 module.exports = repeatString;
