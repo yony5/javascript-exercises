68ddbe11e036445719aa73429b93b8f44ce310fa
yony fernandez  (personal) <yonyfernandezjr@gmail.com>
2023-10-30 01:09:06 -0400

    Completed Unit test 3
    - all tests pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = (wordToReverse) => {
+    let size = wordToReverse.length;
+    let result ="";
+    while(size > 0){
+        result += wordToReverse[size - 1];
+        size--;
+    }
+    return result;
 };
 
 // Do not edit below this line
